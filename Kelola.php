ea10498f3beb3011679440a703c69801b7ea821c
Dimas Shofa Gunarso <55543999+Dimasshogun@users.noreply.github.com>
2023-04-01 16:14:31 +0700

    CRUD PHP MySQL
    CRUD PHP MySQL - 06 - Create dan Delete Data

--- Kelola.php
+++ Kelola.php
@@ -34,23 +34,24 @@
         </div>
     </nav>
     <div class="container">
+        <!-- PAKAI method = POST -->
         <form action="Proses.php" method="POST" id="">
             <div class="mb-3 row">
                 <label for="nisn" class="col-sm-2 col-form-label">NISN</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nisn" placeholder="Contoh : 18090061">
+                    <input type="text" class="form-control" name="nisn" id="nisn" placeholder="Contoh : 18090061">
                 </div>
             </div>
             <div class="mb-3 row">
                 <label for="nama" class="col-sm-2 col-form-label">Nama</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nama" placeholder="Contoh : Dimas Shofa Gunarso">
+                    <input type="text" class="form-control" name="nama" id="nama" placeholder="Contoh : Dimas Shofa Gunarso">
                 </div>
             </div>
             <div class="mb-3 row">
                 <label for="jk" class="col-sm-2 col-form-label">Jenis Kelamin</label>
                 <div class="col-sm-10">
-                    <select id="jk" class="form-select" aria-label="Default select example">
+                    <select name="jenis_kelamin" id="jk" class="form-select" aria-label="Default select example">
                         <option selected>Jenis Kelamin</option>
                         <option value="1">Laki-laki</option>
                         <option value="2">Perempuan</option>
@@ -61,13 +62,13 @@
             <div class="mb-3 row">
                 <label for="foto" class="col-sm-2 col-form-label">Foto Siswa</label>
                 <div class="col-sm-10">
-                    <input class="form-control" type="file" id="foto">
+                    <input class="form-control" type="file" name="foto" id="foto">
                 </div>
             </div>
             <div class="mb-3 row">
                 <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
                 <div class="col-sm-10">
-                    <textarea class="form-control" id="alamat" rows="3" placeholder="Contoh : Jalan Gurami Widuri Pemalang"></textarea>
+                    <textarea class="form-control" name="alamat" id="alamat" rows="3" placeholder="Contoh : Jalan Gurami Widuri Pemalang"></textarea>
                 </div>
             </div>
             <div class="mb-3 row mt-4">
--- Proses.php
+++ Proses.php
@@ -1,13 +1,38 @@
 <?php
+include 'Koneksi.php';
 
 if (isset($_POST['aksi'])) {
     if ($_POST['aksi'] == "add") {
-        echo "Tambah Data <a href='index.php'>Home</a>";
+        $nisn = $_POST['nisn']; //didalam variabel post yaitu "nisn" di ambil dari input name "kelola.php"
+        $nama = $_POST['nama'];
+        $jenis_kelamin = $_POST['jenis_kelamin'];
+        $foto =  "gambar1.jpg";
+        $alamat = $_POST['alamat'];
+
+        $query =  "INSERT INTO tb_siswa VALUES(null, '$nisn' , '$nama' , '$jenis_kelamin' , '$foto' , '$alamat') ";
+        $sql = mysqli_query($conn, $query);
+
+        if ($sql) {
+            header("location:index.php"); //untuk masuk ke index
+            // echo "Berhasil tambah Data <a href='index.php'>Home</a>";
+        } else {
+            echo $query;
+        }
     } elseif ($_POST['aksi'] == "edit") {
         echo "Edit Data <a href='index.php'>Home</a>";
     }
 }
 
 if (isset($_GET['hapus'])) {
-    echo "Hapus Data <a href='index.php'>Home</a>";
+    $id_siswa = $_GET['hapus'];
+    $query = "DELETE FROM tb_siswa WHERE id_siswa = '$id_siswa';";
+    $sql = mysqli_query($conn, $query);
+
+    if ($sql) {
+        header("location:index.php"); //untuk masuk ke index
+        // echo "Berhasil tambah Data <a href='index.php'>Home</a>";
+    } else {
+        echo $query;
+    }
+    // echo "Hapus Data <a href='index.php'>Home</a>";
 }
