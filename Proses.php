0a27da9652833bcf226e71f33b8d11b93aed38ad
Dimas Shofa Gunarso <55543999+Dimasshogun@users.noreply.github.com>
2023-04-11 13:51:34 +0700

    add function
    CRUD PHP MySQL - 09 - Function pada Pemrograman PHP

--- Proses.php
+++ Proses.php
@@ -1,86 +1,33 @@
 <?php
-include 'Koneksi.php';
+include 'Fungsi.php';
 
 if (isset($_POST['aksi'])) {
     if ($_POST['aksi'] == "add") {
 
-        // var_dump($_FILES);
-        // echo $_FILES['foto']['name'];
-        // die();
+        $berhasil = tambah_data($_POST, $_FILES);
 
-        $nisn = $_POST['nisn']; //didalam variabel post yaitu "nisn" di ambil dari input name "kelola.php"
-        $nama_siswa = $_POST['nama_siswa'];
-        $jenis_kelamin = $_POST['jenis_kelamin'];
-        $foto =  $_FILES['foto']['name'];
-        $alamat = $_POST['alamat'];
-
-        $dir = "img/";
-        $tmpFile = $_FILES['foto']['tmp_name'];
-
-        move_uploaded_file($tmpFile, $dir . $foto);
-        // die();
-
-        $query =  "INSERT INTO tb_siswa VALUES(null, '$nisn' , '$nama_siswa' , '$jenis_kelamin' , '$foto' , '$alamat') ";
-        $sql = mysqli_query($conn, $query);
-
-        if ($sql) {
+        if ($berhasil) {
             header("location:index.php"); //untuk masuk ke index
-            // echo "Berhasil tambah Data <a href='index.php'>Home</a>";
         } else {
-            echo $query;
+            echo $berhasil;
         }
     } elseif ($_POST['aksi'] == "edit") {
-        // echo "Edit Data <a href='index.php'>Home</a>";
-        // var_dump($_POST);
-        $id_siswa = $_POST['id_siswa'];
-        $nisn = $_POST['nisn'];
-        $nama_siswa = $_POST['nama_siswa'];
-        $jenis_kelamin = $_POST['jenis_kelamin'];
-        // $foto =  $_FILES['foto']['name'];
-        $alamat = $_POST['alamat'];
-
-        $queryShow = "SELECT * FROM tb_siswa WHERE id_siswa = '$id_siswa';";
-        $sqlShow = mysqli_query($conn, $queryShow);
-        $result = mysqli_fetch_assoc($sqlShow);
-        if ($_FILES['foto']['name'] != "") { //== '' dibawahjuga
-            $foto = $_FILES['foto']['name'];
-            unlink("img/" . $result['foto_siswa']);
-            move_uploaded_file($_FILES['foto']['tmp_name'], 'img/' . $_FILES['foto']['name']);
-            echo "ada file";
-            // echo "kosong";
+        $berhasil = ubah_data($_POST, $_FILES);
+        if ($berhasil) {
+            header("location:index.php"); //untuk masuk ke index
         } else {
-            $foto = $result['foto_siswa']; //ambil dari db
-            echo "kosong";
-            // echo "ada file";
+            echo $berhasil;
         }
-        // die();
-
-        $query = " UPDATE tb_siswa SET nisn='$nisn', nama_siswa='$nama_siswa', jenis_kelamin ='$jenis_kelamin', alamat ='$alamat', foto_siswa ='$foto' WHERE id_siswa= '$id_siswa';";
-        $sql = mysqli_query($conn, $query);
-        header("location:index.php");
     }
 }
 
 if (isset($_GET['hapus'])) {
-    $id_siswa = $_GET['hapus']; //$id_siswa ini ambil dari variabel tomboll hapus di index.php 
-    $queryShow = "SELECT * FROM tb_siswa WHERE id_siswa = '$id_siswa';"; // id_siswa ambil di id db
 
-    $sqlShow = mysqli_query($conn, $queryShow);
-    $result = mysqli_fetch_assoc($sqlShow);
+    $berhasil = hapus_data($_GET);
 
-    // var_dump($result);
-    unlink("img/" . $result['foto_siswa']); //untuk hapus di folder
-    // echo $_FILES['foto']['name'];
-    // die();
-
-    $query = "DELETE FROM tb_siswa WHERE id_siswa = '$id_siswa';";
-    $sql = mysqli_query($conn, $query);
-
-    if ($sql) {
+    if ($berhasil) {
         header("location:index.php"); //untuk masuk ke index
-        // echo "Berhasil tambah Data <a href='index.php'>Home</a>";
     } else {
-        echo $query;
+        echo $berhasil;
     }
-    // echo "Hapus Data <a href='index.php'>Home</a>";
 }
